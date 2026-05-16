e523f1b47f847739efcbc5e7c24683e09a229c85
arvin karimi <hayitsarvink@gmail.com>
2026-05-16 14:50:55 +0330

    added build to build

--- react-router.config.ts
+++ react-router.config.ts
@@ -5,5 +5,5 @@ export default {
   // Server-side render by default, to enable SPA mode set this to `false`
 
   ssr: true,
-  buildDirectory: "dist",
+  // buildDirectory: "dist",
 } satisfies Config;
