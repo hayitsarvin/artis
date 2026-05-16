373aa86fe8f32ee206eae2777ab5aa60a0d7262f
arvin karimi <hayitsarvink@gmail.com>
2026-05-16 12:31:21 +0330

    added build to dist

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
+
   ssr: true,
+  buildDirectory: "dist",
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  build: {
+    outDir: 'dist',      // پوشه خروجی
+    emptyOutDir: true,   // قبل از build پوشه را خالی می‌کند
+  }
 });
